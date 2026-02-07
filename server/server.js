cc0ff57f2a5c462eba32c103e6870385402591ee
Mohmd <m_alsaidi@fanshaweonline.ca>
2026-02-06 20:01:02 -0500

    lab 5

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { initDatabase, deleteDatabase, insertDocument } from './modules/db.js';
+import { initDatabase, deleteDatabase, insertDocument, insertDocuments } from './modules/db.js';
+import * as fs from "node:fs/promises";
 
 let db = undefined;
 try {
@@ -17,6 +18,16 @@ try {
     console.log("deleteDatabase result: ", result);
     result = await insertDocument(db, "project-1", 'raw_advisories', data);
     console.log("insertDocument result: ", result);
+
+let isoFile = await fs.readFile(process.env.ISO_FILE_PATH);
+    let isoText = await isoFile.toString();
+    let isoCountries = await JSON.parse(isoText);
+
+    // Write to the database
+    result = await insertDocuments(db, "project-1", "iso_countries", isoCountries);
+    console.log(result.insertedCount, "country codes loaded");
+
+
 }
 catch (e) {
     console.error(e);
--- server/tests/db-query.js
+++ server/tests/db-query.js
@@ -18,3 +18,5 @@ try {
 finally {
     client.close();
 }
+
+
