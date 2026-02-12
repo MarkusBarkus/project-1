57085de8c990fd19c11b91479b207617bf0f46f0
Mohmd <m_alsaidi@fanshaweonline.ca>
2026-02-12 18:00:31 -0500

    Adds Material UI and updates App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Paper,
+  Button
+} from "@mui/material";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (<>
+    <AppBar position="sticky">
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="h6">
+          INFO-3139 P1 ( M_Alsaidi )
+        </Typography>
+      </Toolbar >
+    </AppBar >
+    <Paper elevation={4} sx={{ marginTop: "0.5em", padding: "1em" }}>
+      <Button variant="contained">Refresh Database</Button>
+    </Paper>
+  </>);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
